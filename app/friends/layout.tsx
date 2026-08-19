2e166d8ba6d0f1aa306a721e310109e09478790d
kameronshaw13 <kameronshaw13@gmail.com>
2026-08-19 18:42:05 -0500

    Add Friends app metadata

---
+++ app/friends/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "Friends Pick'em",
+  description: "Private Friends football pick'em.",
+  applicationName: "Friends Pick'em",
+  appleWebApp: { capable: true, title: "Friends Pick'em", statusBarStyle: "black-translucent" },
+  icons: { icon: "/football-icon.svg", apple: "/football-icon.svg" },
+  manifest: "/friends/manifest.webmanifest"
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: "#20282d"
+};
+
+export default function FriendsLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
