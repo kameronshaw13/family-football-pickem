c45c504ab716d4bb3f3343127bc9d11ee7a5de7d
kameronshaw13 <kameronshaw13@gmail.com>
2026-08-19 19:25:53 -0500

    Bind Friends route permanently

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,5 +1,7 @@
+import CompanionAppEnhancements from "@/components/CompanionAppEnhancements";
 import PickemApp from "@/components/PickemApp";
+import RouteAppBootstrap from "@/components/RouteAppBootstrap";
 
 export default function FriendsPickemPage() {
-  return <div className="route-app group-friends"><PickemApp /></div>;
+  return <div className="route-app group-friends"><RouteAppBootstrap slug="friends" /><PickemApp /><CompanionAppEnhancements slug="friends" /></div>;
 }
