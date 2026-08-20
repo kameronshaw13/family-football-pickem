4834ec7f683a5586230aca245907afc2eca66e4e
kameronshaw13 <kameronshaw13@gmail.com>
2026-08-19 19:26:05 -0500

    Bind Other Family route permanently

--- app/other-family/page.tsx
+++ app/other-family/page.tsx
@@ -1,5 +1,7 @@
+import CompanionAppEnhancements from "@/components/CompanionAppEnhancements";
 import PickemApp from "@/components/PickemApp";
+import RouteAppBootstrap from "@/components/RouteAppBootstrap";
 
 export default function OtherFamilyPickemPage() {
-  return <div className="route-app group-other-family"><PickemApp /></div>;
+  return <div className="route-app group-other-family"><RouteAppBootstrap slug="other-family" /><PickemApp /><CompanionAppEnhancements slug="other-family" /></div>;
 }
