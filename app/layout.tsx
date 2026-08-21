447ba5c761205ed9afd3b484e3f41e0c8e4d9567
kameronshaw13 <kameronshaw13@gmail.com>
2026-08-21 16:43:16 -0500

    Load side bet display guard

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import AppExperienceEnhancements from "@/components/AppExperienceEnhancements";
 import AppUiCoordinator from "@/components/AppUiCoordinator";
 import DogPickAdjustmentAlerts from "@/components/DogPickAdjustmentAlerts";
 import PlayerProfiles from "@/components/PlayerProfiles";
+import SideBetDisplayGuard from "@/components/SideBetDisplayGuard";
 
 export const metadata: Metadata = {
   title: "Family Football Pick'em",
@@ -36,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preload" href="/football-pickem-wordmark.png" as="image" type="image/png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600;700;800;900&display=swap" />
       </head>
-      <body>{children}<AppExperienceEnhancements /><AppUiCoordinator /><DogPickAdjustmentAlerts /><PlayerProfiles /></body>
+      <body>{children}<AppExperienceEnhancements /><AppUiCoordinator /><DogPickAdjustmentAlerts /><PlayerProfiles /><SideBetDisplayGuard /></body>
     </html>
   );
 }
