1172fc74dd44cd239bc83d9b4b312593a6e38cbc
kameronshaw13 <kameronshaw13@gmail.com>
2026-08-21 12:00:24 -0500

    Add Caleb Family route

---
+++ app/caleb-family/page.tsx
@@ -0,0 +1,6 @@
+import PickemApp from "@/components/PickemApp";
+import RouteAppBootstrap from "@/components/RouteAppBootstrap";
+
+export default function CalebFamilyPickemPage() {
+  return <div className="route-app group-other-family"><RouteAppBootstrap slug="other-family" /><PickemApp appSlug="other-family" /></div>;
+}
