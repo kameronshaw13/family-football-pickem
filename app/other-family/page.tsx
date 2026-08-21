2d57297f82130b3c92bb30162d4a174c3f1ed8e7
kameronshaw13 <kameronshaw13@gmail.com>
2026-08-21 12:01:48 -0500

    Remove old Other Family page route

--- app/other-family/page.tsx
+++
@@ -1,6 +0,0 @@
-import PickemApp from "@/components/PickemApp";
-import RouteAppBootstrap from "@/components/RouteAppBootstrap";
-
-export default function OtherFamilyPickemPage() {
-  return <div className="route-app group-other-family"><RouteAppBootstrap slug="other-family" /><PickemApp appSlug="other-family" /></div>;
-}
