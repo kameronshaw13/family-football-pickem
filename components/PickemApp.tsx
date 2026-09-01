04809937789bc068ab736a91b5a7c8d71606a2c8
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-01 10:12:40 -0500

    Load week scope and manual lock enhancements

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -4,7 +4,8 @@ import type { AppSlug } from "@/lib/rulePresentation";
 import PickemAppBase from "@/components/PickemAppBase";
 import Batch1UiEnhancements from "@/components/Batch1UiEnhancements";
 import Batch1bSideBetStyles from "@/components/Batch1bSideBetStyles";
+import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /></>;
+  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /></>;
 }
