c317cea77e2d92134331dc19219270724616312e
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-01 22:40:28 -0500

    Load app pass fixes

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -5,7 +5,8 @@ import PickemAppBase from "@/components/PickemAppBase";
 import Batch1UiEnhancements from "@/components/Batch1UiEnhancements";
 import Batch1bSideBetStyles from "@/components/Batch1bSideBetStyles";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
+import AppPassFixes from "@/components/AppPassFixes";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /></>;
+  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><AppPassFixes appSlug={appSlug} /></>;
 }
