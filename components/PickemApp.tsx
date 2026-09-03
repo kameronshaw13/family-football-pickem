3305ce0a92c5a1e78d4938986dc557898216be9c
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-03 09:06:55 -0500

    Remove style-only UI enhancement component

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -2,7 +2,6 @@
 
 import type { AppSlug } from "@/lib/rulePresentation";
 import PickemAppBase from "@/components/PickemAppBase";
-import Batch1UiEnhancements from "@/components/Batch1UiEnhancements";
 import Batch1bSideBetStyles from "@/components/Batch1bSideBetStyles";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
 import MyCardPrepaintStabilizer from "@/components/MyCardPrepaintStabilizer";
@@ -10,5 +9,5 @@ import AppPassFixes from "@/components/AppPassFixes";
 import DateHeaderOrdinals from "@/components/DateHeaderOrdinals";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
+  return <><PickemAppBase appSlug={appSlug} /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
 }
