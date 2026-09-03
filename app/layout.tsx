a00858046ed4b14e50b861c64a38ab0fcd3cd0eb
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-02 23:23:15 -0500

    Revert app font to Roboto Slab

--- app/layout.tsx
+++ app/layout.tsx
@@ -138,7 +138,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const espnCollegeAbbreviations = await fetchEspnCollegeAbbreviationAliases().catch(() => ({}));
 
   return (
-    <html lang="en" data-theme="light" style={{ "--font-display": '"Roboto Condensed", "Arial Narrow", Arial, sans-serif' } as React.CSSProperties}>
+    <html lang="en" data-theme="light">
       <head>
         <script dangerouslySetInnerHTML={{ __html: abbreviationBootstrapScript(espnCollegeAbbreviations) }} />
         <script dangerouslySetInnerHTML={{ __html: SESSION_RECOVERY_SCRIPT }} />
@@ -147,7 +147,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preload" href="/header-wordmark.png" as="image" type="image/png" />
         <link rel="preload" href="/football-pickem-wordmark.png" as="image" type="image/png" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;500;600;700;800;900&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600;700;800;900&display=swap" />
       </head>
       <body>{children}<AppExperienceEnhancements /><AppUiCoordinator /><DogPickAdjustmentAlerts /><PlayerProfiles /></body>
     </html>
