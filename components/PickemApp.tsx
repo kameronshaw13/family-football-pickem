f6e1fc5098bd53b3b72a3b2985e7baee901989f5
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-03 09:53:56 -0500

    Remove separate My Card stabilizer mount

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -3,10 +3,9 @@
 import type { AppSlug } from "@/lib/rulePresentation";
 import PickemAppBase from "@/components/PickemAppBase";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
-import MyCardPrepaintStabilizer from "@/components/MyCardPrepaintStabilizer";
 import AppPassFixes from "@/components/AppPassFixes";
 import DateHeaderOrdinals from "@/components/DateHeaderOrdinals";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
+  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
 }
