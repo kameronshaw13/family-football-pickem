2aa3906b1c851f491dc398cd03bcd8dfb72bedbc
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-03 08:48:42 -0500

    Remove dynamic latest UI patch layer

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -7,9 +7,8 @@ import Batch1bSideBetStyles from "@/components/Batch1bSideBetStyles";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
 import MyCardPrepaintStabilizer from "@/components/MyCardPrepaintStabilizer";
 import AppPassFixes from "@/components/AppPassFixes";
-import LatestUiFixes from "@/components/LatestUiFixes";
 import DateHeaderOrdinals from "@/components/DateHeaderOrdinals";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><LatestUiFixes /><DateHeaderOrdinals /></>;
+  return <><PickemAppBase appSlug={appSlug} /><Batch1UiEnhancements /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
 }
