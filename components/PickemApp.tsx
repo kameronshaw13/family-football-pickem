675f43be4a47550297eaae029fbba44c0a85c701
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-03 09:08:38 -0500

    Remove side bet style observer component

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -2,12 +2,11 @@
 
 import type { AppSlug } from "@/lib/rulePresentation";
 import PickemAppBase from "@/components/PickemAppBase";
-import Batch1bSideBetStyles from "@/components/Batch1bSideBetStyles";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
 import MyCardPrepaintStabilizer from "@/components/MyCardPrepaintStabilizer";
 import AppPassFixes from "@/components/AppPassFixes";
 import DateHeaderOrdinals from "@/components/DateHeaderOrdinals";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><Batch1bSideBetStyles /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
+  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><MyCardPrepaintStabilizer /><AppPassFixes appSlug={appSlug} /><DateHeaderOrdinals /></>;
 }
