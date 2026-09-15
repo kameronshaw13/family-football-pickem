a65a471d63f38d65e070044687888ca12253b9c9
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-15 09:37:17 -0500

    Render lock icon only after confirmed lock

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -4,7 +4,8 @@ import type { AppSlug } from "@/lib/rulePresentation";
 import PickemAppBase from "@/components/PickemAppBase";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
 import PickemUiCorrections from "@/components/PickemUiCorrections";
+import LockedPickIconSync from "@/components/LockedPickIconSync";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><PickemUiCorrections appSlug={appSlug} /></>;
+  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><PickemUiCorrections appSlug={appSlug} /><LockedPickIconSync appSlug={appSlug} /></>;
 }
