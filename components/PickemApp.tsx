1005e5975e0968966e0fbd0a5fe48f6932117f15
kameronshaw13 <kameronshaw13@gmail.com>
2026-09-15 09:21:38 -0500

    Apply lock icon and Friends side bet UI corrections

--- components/PickemApp.tsx
+++ components/PickemApp.tsx
@@ -3,7 +3,8 @@
 import type { AppSlug } from "@/lib/rulePresentation";
 import PickemAppBase from "@/components/PickemAppBase";
 import WeekScopeAndManualLockEnhancements from "@/components/WeekScopeAndManualLockEnhancements";
+import PickemUiCorrections from "@/components/PickemUiCorrections";
 
 export default function PickemApp({ appSlug = "shaw-family" }: { appSlug?: AppSlug }) {
-  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /></>;
+  return <><PickemAppBase appSlug={appSlug} /><WeekScopeAndManualLockEnhancements appSlug={appSlug} /><PickemUiCorrections appSlug={appSlug} /></>;
 }
